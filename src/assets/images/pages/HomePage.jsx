5b62c9a56385389870cdd7fa0877eaf8ed3d5576
NarendraSaktiWw <yurayurawa@gmail.com>
2025-09-19 15:34:44 +0700

    Landingpage

--- src/assets/images/pages/HomePage.jsx
+++ src/assets/images/pages/HomePage.jsx
@@ -1,6 +1,17 @@
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className="homepage">
+      <div className="container">
+        <div className="hero">
+          <div className="box">
+            <h1>belajar membuat website dengan <span></span>
+            </h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores tempora voluptates veniam porro ipsa itaque iure modi, reprehenderit similique in iusto unde quae! Porro facere, vitae a modi odit cum.</p>
+            <a href="#">tentang kami</a>
+          </div>
+  </div>
+</div>
+    </div>
   )
 }
 
